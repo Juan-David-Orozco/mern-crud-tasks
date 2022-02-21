67f87568f095a11508e5f87918ab774bddd7592b
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-02-20 21:48:30 -0500

    axios y peticion get

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Navigation />
-      <div>
+      <div className='container p-4'>
         <Route path="/" exact component={NotesList} />
         <Route path="/edit/:id" component={CreateNote} />
         <Route path="/create" component={CreateNote} />
